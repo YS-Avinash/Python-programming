34ff499bd239ed69fd5c5e8eeb3732c62a823916
Yoga Sai Avinash <yogasaiavinash@gmail.com>
2020-03-12 20:19:08 +0530

    Python Fundamentals Problem 7

--- SeedNumber.py
+++ SeedNumber.py
@@ -4,11 +4,10 @@ A number X is said to be a seed of number Y, if multiplying X by its each digit 
 For example, 123 is a seed of 738 as 123*1*2*3 = 738.
 """
 def seed_no(number,ref_no):
-    #start writing your code here
     prod = 1
     for digit in str(number):
         prod=prod*int(digit)
     return prod*number == ref_no
-number=123
-ref_no=738
+#Let input be 123,738
+number,ref_no = list(map(int,input().split(",")))
 print(seed_no(number,ref_no))
