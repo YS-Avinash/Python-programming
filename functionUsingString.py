a72daaf4b0dc1718b7d0d4e4fea9645a347767d5
Yoga Sai Avinash <yogasaiavinash@gmail.com>
2020-03-12 20:22:22 +0530

    Python Fundamentals Problem 1

--- functionUsingString.py
+++ functionUsingString.py
@@ -5,5 +5,5 @@
 def add_string(str1):
     return str1 if len(str1)<3 else str1+"ly" if str1.endswith("ing") else str1+"ing"
 
-str1=input("Enter a string : ")
-print(add_string(str1))
+string=input("Enter a string : ")
+print(add_string(string))
