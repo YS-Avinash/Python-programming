592609e83b87b5f5ef9f47d4beac28072137cc6c
Yoga Sai Avinash <yogasaiavinash@gmail.com>
2020-03-12 20:31:19 +0530

    Python Fundamentals Problem 9

--- dictionaryCreation.py
+++ dictionaryCreation.py
@@ -1,5 +1,7 @@
 '''Python function which generates 
-and returns a dictionary where the keys are numbers between 1 and n (both inclusive) and the values are square of keys.'''
+and returns a dictionary where the keys are numbers between 1 and n (both inclusive) and the values are square of keys.
+if input is 10, then the output is to be {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100}
+'''
 
 def generate_dict(number):
 	#start writing your code here
