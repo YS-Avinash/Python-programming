87fd6f6b98fa17a3fcd6091a3b5281d789ddf468
Yoga Sai Avinash <yogasaiavinash@gmail.com>
2020-03-12 20:34:35 +0530

    Python Fundamentals Problem 11

--- upperLowerAlpha.py
+++ upperLowerAlpha.py
@@ -1,7 +1,6 @@
 '''Find count of caps and small letters in a string '''
 
 def find_upper_and_lower(sentence):
-    #start writing your code here
     result_list=[]
     result_list.append(len([" " for i in sentence if i.isupper()]))
     result_list.append(len([" " for i in sentence if i.islower()]))
