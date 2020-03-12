c54a0beffc351f1ab25d24dc849e19b0b4f45a00
Yoga Sai Avinash <yogasaiavinash@gmail.com>
2020-03-12 19:59:45 +0530

    Python Fundamentals Problem 4

--- FindNine.py
+++ FindNine.py
@@ -2,7 +2,6 @@
 The length of the list can be less than 4 also."""
 
 def find_nine(nums):
-    #Remove pass and write your code here
 	return 9 in (nums if len(nums)<5 else nums[:4])
 
 nums=[1,9,4,5,6]
